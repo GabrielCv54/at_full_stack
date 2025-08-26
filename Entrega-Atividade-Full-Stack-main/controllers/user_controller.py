8f7f0bc32d9817f9ea80cbe8e9f8762a9bfd2a1a
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-08-26 19:24:06 -0300

    feat:adicionando ocontroller de user

---
+++ Entrega-Atividade-Full-Stack-main/controllers/user_controller.py
@@ -0,0 +1,41 @@
+from App import app
+from flask import request,jsonify
+
+users = {}
+id = 1
+@app.route("/users", methods=["POST"])
+def criar_usuario(users):
+    global id
+    dict = request.json
+    user = {"id": id, "nome": dict["nome"], "email": dict["email"]}
+    users[id] = user
+    id += 1
+    return jsonify(user),201
+
+@app.route("/users", methods =["GET"])
+def listar_usuarios():
+    return jsonify (list(users.values())), 200
+
+@app.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = users.get(user_id)
+    if user:
+        return jsonify(user), 200
+    return jsonify({"error": "Usuário não encontrado"}), 404
+
+@app.route("/users/<int:user_id>", methods=["PUT"])
+def atulizar_usuario(user_id):
+    data = request.json
+    user = users.get(user_id)
+    if not user:
+        return jsonify({"error": "Usuário não encontrado"}), 404
+    user["nome"] = data.get("nome", user["nome"])
+    user["email"] = data.get("email", user["email"])
+    return jsonify(user), 200
+
+@app.route("/users/<int:user_id>", methods=["DELETE"])
+def deletar_usuario(user_id):
+    if user_id in users:
+        del users[user_id]
+        return jsonify({"message": "Usuário excluído com sucesso"}), 200
+    return jsonify({"error": "Usuário não encontrado"}), 404
